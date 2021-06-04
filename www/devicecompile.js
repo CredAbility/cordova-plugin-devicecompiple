4c3f6feed6aa02f65cdcd6585a94d090fe3653ac
CredAbility <58071728+CredAbility@users.noreply.github.com>
2021-06-04 14:56:26 +0100

    bug fixes

--- www/devicecompile.js
+++ www/devicecompile.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (success, error) {
-    exec(success, error, 'devicecompile', 'checkDevice', null);
+    exec(success, error, 'devicecompile', 'checkDevice', []);
 };
